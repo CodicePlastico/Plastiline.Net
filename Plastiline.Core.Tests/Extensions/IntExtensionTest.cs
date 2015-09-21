8c85cb377252722f687c163ebfc40930db990d83
Pietro Martinelli <amicofragile@gmail.com>
2015-09-21 21:23:01 +0200

    First commit: nuspec, solution and project files FEATURE IntExtension.Times

---
+++ Plastiline.Core.Tests/Extensions/IntExtensionTest.cs
@@ -0,0 +1,26 @@
+﻿using Plastiline.Core.Extensions;
+using Xunit;
+
+namespace Plastiline.Core.Tests.Extensions
+{
+    public class IntExtensionTest
+    {
+        [Fact]
+        public void TestIteratingFromOneToNWithTimes()
+        {
+            string accumulator = "";
+            int n = 5;
+            n.Times( i => accumulator = accumulator + i.ToString());
+            Assert.Equal("12345", accumulator);
+        }
+
+        [Fact]
+        public void NoIterationWhenThisIsZero()
+        {
+            string accumulator = "";
+            int n = 0;
+            n.Times(i => accumulator = accumulator + i.ToString());
+            Assert.Equal("", accumulator);
+        }
+    }
+}
---
+++ Plastiline.Core/Extensions/IntExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plastiline.Core.Extensions
+{
+    public static class IntExtension
+    {
+        public static void Times(this int n, Action<int> action)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                action(i);
+            }
+        }
+    }
+}
