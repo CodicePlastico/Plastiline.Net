9ebaac2db8b58b5c2a827db8c4cfac082de4d656
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-09-23 08:29:27 +0200

    FEATURE DateTimeExtension.Epoch

---
+++ Plastiline.Core.Tests/Extensions/DateTimeExtensionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plastiline.Core.Extensions;
+using Xunit;
+
+namespace Plastiline.Core.Tests.Extensions
+{
+    public class DateTimeExtensionTest
+    {
+        [Fact]
+        public void EpochFor19700101OnUtcIs0()
+        {
+            DateTime startOfEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            Assert.Equal(0, startOfEpoch.Epoch());
+        }
+
+        [Fact]
+        public void EpochFor19700102OnUtcIs86400000()
+        {
+            DateTime startOfEpoch = new DateTime(1970, 1, 2, 0, 0, 0, DateTimeKind.Utc);
+            Assert.Equal(86400000, startOfEpoch.Epoch());
+        }
+
+        [Fact]
+        public void EpochFor19700101OnLocalTimeZoneIs0()
+        {
+            DateTime startOfEpoch = new DateTime(1969, 12, 31, 23, 0, 0, DateTimeKind.Utc);
+            TimeZoneInfo info = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            DateTime centralEurope = TimeZoneInfo.ConvertTime(startOfEpoch, info);
+            Assert.Equal(-3600000, centralEurope.Epoch());
+        }
+    }
+}
---
+++ Plastiline.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Plastiline.Core.Extensions
+{
+    public static class DateTimeExtension
+    {
+        public static long Epoch(this DateTime date)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64((date.ToUniversalTime() - epoch).TotalMilliseconds);
+        }
+    }
+}
