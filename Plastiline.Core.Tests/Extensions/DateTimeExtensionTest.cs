f834f22d188efd7ec3444b1744dc942565061990
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-09-23 08:34:05 +0200

    FEATURE DateTimeExtension.ToDateTime

--- Plastiline.Core.Tests/Extensions/DateTimeExtensionTest.cs
+++ Plastiline.Core.Tests/Extensions/DateTimeExtensionTest.cs
@@ -10,27 +10,40 @@ namespace Plastiline.Core.Tests.Extensions
 {
     public class DateTimeExtensionTest
     {
+        private static readonly DateTime StartOfEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         [Fact]
         public void EpochFor19700101OnUtcIs0()
         {
-            DateTime startOfEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            Assert.Equal(0, startOfEpoch.Epoch());
+            Assert.Equal(0, StartOfEpoch.Epoch());
         }
 
         [Fact]
         public void EpochFor19700102OnUtcIs86400000()
         {
-            DateTime startOfEpoch = new DateTime(1970, 1, 2, 0, 0, 0, DateTimeKind.Utc);
-            Assert.Equal(86400000, startOfEpoch.Epoch());
+            DateTime aDayLater = new DateTime(1970, 1, 2, 0, 0, 0, DateTimeKind.Utc);
+            Assert.Equal(86400000, aDayLater.Epoch());
         }
 
         [Fact]
         public void EpochFor19700101OnLocalTimeZoneIs0()
         {
-            DateTime startOfEpoch = new DateTime(1969, 12, 31, 23, 0, 0, DateTimeKind.Utc);
+            DateTime oneHourAgo = new DateTime(1969, 12, 31, 23, 0, 0, DateTimeKind.Utc);
             TimeZoneInfo info = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-            DateTime centralEurope = TimeZoneInfo.ConvertTime(startOfEpoch, info);
+            DateTime centralEurope = TimeZoneInfo.ConvertTime(oneHourAgo, info);
             Assert.Equal(-3600000, centralEurope.Epoch());
+        }
+
+        [Fact]
+        public void ZeroIsEpochSource()
+        {
+            Assert.Equal(StartOfEpoch, ((long)0).ToDateTime());
+        }
+
+        [Fact]
+        public void LongValueIsEpochOffset()
+        {
+            DateTime oneHourAgo = new DateTime(1969, 12, 31, 23, 0, 0, DateTimeKind.Utc);
+            Assert.Equal(oneHourAgo, ((long)-3600000).ToDateTime());
         }
     }
 }
--- Plastiline.Core/Extensions/DateTimeExtension.cs
+++ Plastiline.Core/Extensions/DateTimeExtension.cs
@@ -9,5 +9,11 @@ namespace Plastiline.Core.Extensions
             DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             return Convert.ToInt64((date.ToUniversalTime() - epoch).TotalMilliseconds);
         }
+
+        public static DateTime ToDateTime(this long unixTime)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddMilliseconds(unixTime);
+        }
     }
 }
