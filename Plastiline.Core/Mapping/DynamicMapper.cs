f5482b1ff8718071e5c83377baf45fbd2439d5d6
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-09-25 08:39:18 +0200

    FEATURE Null-safe dynamic to short helper methods

---
+++ Plastiline.Core/Mapping/DynamicMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Plastiline.Core.Mapping
+{
+    public static class DynamicMapper
+    {
+        public static short? ToShort(dynamic d)
+        {
+            return d != null ? short.Parse(d.ToString()) : null;
+        }
+    }
+}
