f71852cb6a15086dc7af994485f57b334ee5a30d
Pietro Martinelli <pietro.martinelli@codiceplastico.com>
2015-09-25 08:48:37 +0200

    FEATURE Extension methods string.CapFirst, string.UncapFirst

---
+++ Plastiline.Core/Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Plastiline.Core.Extensions
+{
+    public static class StringExtension
+    {
+        private static string UpdateFirst(string arg, Func<string, string> updater)
+        {
+            string result = arg;
+            if (!string.IsNullOrEmpty(arg))
+            {
+                result = updater(result.Substring(0, 1).ToUpper()) + result.Substring(1, result.Length - 1);
+            }
+            return result;
+        }
+
+        public static string CapFirst(this string self)
+        {
+            return UpdateFirst(self, s => s.ToUpper());
+        }
+
+        public static string UncapFirst(this string self)
+        {
+            return UpdateFirst(self, s => s.ToLower());
+        }
+    }
+}
